1ff882c0b1ea4a4ed9fd1268b166bd250fd9ae64
asansone26 <asansone@studenti.apuliadigitalmaker.it>
2022-03-10 13:07:28 +0100

    ciao

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+type Car = {
+    id: number | null;
+    marca: string | null;
+    modello: string | null;
+
+}
+var cars: Car[];
+cars = [{ id: 1, marca: "fiat", modello: "bravo" }, { id: 2, marca: "ford", modello: "kuga" }, { id: 3, marca: "lancia", modello: "y" }, { id: 4, marca: "lamborghini", modello: "elett" }, { id: 5, marca: "ferrari", modello: "bomba" }]
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+async function stampa() {
+
+    for (let i = 0; i < cars.length; i++) {
+        console.log(cars[i])
+        await delay(2000);
+    }
+    console.log("fine popolamento");
+}
+console.log("inizio popolamento");
+stampa();
+
+
+
+
+
+
+    
+
+
+
+    
+     
+
+
