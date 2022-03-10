66177646b1a125117636bafaea618c416ac76e82
asansone26 <asansone@studenti.apuliadigitalmaker.it>
2022-03-10 13:59:47 +0100

    ciao

---
+++ src/formegeom.ts
@@ -0,0 +1,27 @@
+type Quadrato = {
+    lato1: Number;
+    
+
+}
+type Rettangolo = {
+    base: Number;
+    altezza: Number;
+
+}
+type Triangolo = {
+    base: Number;
+    altezza: Number;
+
+}
+type Cerchio = {
+    raggio: Number;
+    
+
+}
+
+var lat1 = 2
+var lat2 =3
+var tot= lat1 * lat2
+console.log(tot)
+
+
